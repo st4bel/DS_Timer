181852d659f7b9e7079f17f05133f58639a1de5e
Hans Kirchner <hans-kirchner@gmx.de>
2016-04-16 20:55:40 +0200

    Add kekse parser

---
+++ kekse.py
@@ -0,0 +1,21 @@
+import re
+
+FORMAT = re.compile(r"\[([a-zA-Z\.\-0-9]+)\|([a-zA-Z0-9%:]+)\|([a-zA-Z0-9]{8})\]")
+
+def java_hash_code(str):
+    """Recompute the hash given in an exported. TODO This does not work as expected."""
+    hash = 0
+    if len(str) == 0:
+        return hash
+    for c in str:
+        hash = ((hash << 5) - hash) + ord(c)
+        # convert hash to int32
+    return abs(hash)
+
+def parse(str):
+    """Parse an exported session cookie string from DS Kekse."""
+    match = FORMAT.search(str)
+    if match:
+        return dict(domain=match.group(1), sid=match.group(2))
+    else:
+        return None
