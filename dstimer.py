10bf2e8bb0cc38697947fa5641cf440fdef20b06
Hans Kirchner <hans-kirchner@gmx.de>
2016-05-02 14:47:49 +0200

    Add intelli units function and tests

--- dstimer.py
+++ dstimer.py
@@ -63,6 +63,40 @@ def get_confirm_screen(session, domain, form, units, target_x, target_y, type):
 def just_do_it(session, domain, action, data):
     response = session.post("https://" + domain + action, data=data)
 
+def intelli_single(format, actual):
+    format = str(format)
+    # Format: x
+    match = re.fullmatch(r"[0-9]+", format)
+    if match is not None:
+        val = int(format)
+        return min(val, actual)
+    # Format: *
+    if format == "*":
+        return actual
+    # Format: >=x
+    match = re.fullmatch(r">=([0-9]+)", format)
+    if match is not None:
+        requested = int(match.group(1))
+        if actual >= requested:
+            return actual
+        else:
+            return None
+    # Format: =x
+    match = re.fullmatch(r"=([0-9]+)", format)
+    if match is not None:
+        requested = int(match.group(1))
+        if actual >= requested:
+            return requested
+        else:
+            return None
+    # Format: -x
+    match = re.fullmatch(r"-([0-9]+)", format)
+    if match is not None:
+        reduce = int(match.group(1))
+        return max(actual - reduce, 0)
+    # Error
+    raise ValueError("Unknown format: {0}".format(format))
+
 def get_local_offset():
     client = ntplib.NTPClient()
     response = client.request("europe.pool.ntp.org", version=3)
---
+++ dstimer_test.py
@@ -0,0 +1,29 @@
+import unittest
+from dstimer import intelli_single
+
+class IntelliSingleTest(unittest.TestCase):
+    def test_default(self):
+        self.assertEqual(intelli_single("100", 101), 100)
+        self.assertEqual(intelli_single("100", 100), 100)
+        self.assertEqual(intelli_single("100", 99), 99)
+
+    def test_star(self):
+        self.assertEqual(intelli_single("*", 100), 100)
+
+    def test_gte(self):
+        self.assertEqual(intelli_single(">=100", 101), 101)
+        self.assertEqual(intelli_single(">=100", 100), 100)
+        self.assertEqual(intelli_single(">=100", 99), None)
+
+    def test_eq(self):
+        self.assertEqual(intelli_single("=100", 101), 100)
+        self.assertEqual(intelli_single("=100", 100), 100)
+        self.assertEqual(intelli_single("=100", 99), None)
+
+    def test_minus(self):
+        self.assertEqual(intelli_single("-50", 51), 1)
+        self.assertEqual(intelli_single("-50", 50), 0)
+        self.assertEqual(intelli_single("-50", 49), 0)
+
+if __name__ == "__main__":
+    unittest.main()
