3cd9bed3298b710cafad87d2f632bf5552cc131f
Hans Kirchner <hans-kirchner@gmx.de>
2016-10-27 17:07:33 +0200

    Add argument --open-browser

--- dstimer_server.py
+++ dstimer_server.py
@@ -5,12 +5,14 @@ from logging import handlers
 import os, sys
 from pythonjsonlogger import jsonlogger
 import argparse
+import webbrowser
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Attack Helper for Die Staemme")
     parser.add_argument("--host", help="Bind the server on this address", default="127.0.0.1")
     parser.add_argument("--port", help="Bind the server on this port", default=5000)
     parser.add_argument("--allow-root", help="Allow to start server with root or sudo", action="store_true")
+    parser.add_argument("--open-browser", help="Open browser tab with dashboard after startup", action="store_true")
     args = parser.parse_args()
 
     if "getuid" in dir(os) and os.getuid() == 0 and not args.allow_root:
@@ -27,6 +29,9 @@ if __name__ == "__main__":
     formatter = jsonlogger.JsonFormatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     handler.setFormatter(formatter)
     logger.addHandler(handler)
+
+    if args.open_browser:
+        webbrowser.open("http://127.0.0.1:" + str(args.port), new=2)
 
     send_action.DaemonThread().start()
     server.app.run(host=args.host, port=args.port)
